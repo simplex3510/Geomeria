e480caa6c26278d42f057dc15269afbb500753b5
simplex3510 <leeyj6457@naver.com>
2021-10-27 19:05:50 +0900

    카메라 이펙트 구현 중

--- Assets/Scripts/MainGame/CameraManager.cs
+++ Assets/Scripts/MainGame/CameraManager.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    float m_zoomIn = 10f;
-    float m_zoomOut = 13f;
-    float m_zoomPower = 0.1f;
     public static bool isZoom
     {
         get;
         set;
     }
+
+    float fullChargeTime = 1f;
+    float currentChargeTime;
+    float zoomIn = 10f;
+    float zoomOut = 13f;
+    float zoomSpeed = 0.1f;
+    
 
     Transform playerPosition;
     Camera mainCamera;
@@ -22,24 +26,45 @@ public class CameraManager : MonoBehaviour
         mainCamera = Camera.main;
         mainCamera.orthographicSize = 5f;
         playerPosition = GetComponent<Transform>();
-        
     }
 
     private void Update()
     {
+        #region camera view
         mainCamera.transform.position = new Vector3(playerPosition.position.x, playerPosition.position.y, -10f);
-    }
+        #endregion
 
-    private void LateUpdate()
-    {
+        #region charge effect
+        if (Input.GetMouseButton(0))
+        {
+            currentChargeTime += Time.deltaTime;
+            if (fullChargeTime <= currentChargeTime)
+            {
+                Debug.Log("mainCamera.orthographicSize = 12f;");
+                mainCamera.orthographicSize = 12f;
+            }
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            currentChargeTime = 0f;
+        }
+        #endregion
+
         if (isZoom)
         {
-            mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, m_zoomIn, m_zoomPower);
+            CameraZoomEffect(zoomIn, zoomSpeed);
         }
         else
         {
-            mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, m_zoomOut, m_zoomPower);
+            CameraZoomEffect(zoomOut, zoomSpeed);
         }
+
+    }
+
+    void CameraZoomEffect(float _zoom, float _zoomSpeed)
+    {
+        mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, _zoom, _zoomSpeed);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
--- Assets/Scripts/MainGame/Player.cs
+++ Assets/Scripts/MainGame/Player.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Player : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class Player : MonoBehaviour
 {
-    public ParticleSystem chargeEffect;
-    public float moveSpeed;
+    public ParticleSystem chargingEffect;
+    public ParticleSystem chargedEffect;
+    public float speed;
 
-    bool isMove;
+    float currentChargeTime;
+    float fullChargeTime = 1f;
     Camera m_camera;
     Rigidbody2D m_rigidbody2D;
+    SpriteRenderer spriteRenderer;
 
     Vector2 minDistance = new Vector2(-3, -3);
     Vector2 maxDistance = new Vector2(3, 3);
-    Vector2 force;
+    Vector2 distance;
     Vector3 startPoint;
     Vector3 endPoint;
 
@@ -22,6 +25,7 @@ public class Player : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         m_camera = Camera.main;
         m_rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -31,14 +35,28 @@ public class Player : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
             startPoint = m_camera.ScreenToWorldPoint(Input.mousePosition);
             startPoint.z = 1f;
 
-            chargeEffect.loop = true;
-            chargeEffect.Play();
-            // Debug.Log("startPoint: " + startPoint.ToString());
+            // var chargingEffectMain = chargingEffect.main;
+            // chargingEffectMain.loop = true;
+            chargingEffect.Play();
         }
 
         if (Input.GetMouseButton(0))
         {
-            // chargeEffect.Play();
+            currentChargeTime += Time.deltaTime;
+
+            if (fullChargeTime - 0.15f <= currentChargeTime)
+            {
+                // spriteRenderer.sprite = Resources.Load<Sprite>("Sprites/Player_Charged");
+                // var chargingEffectMain = chargingEffect.main;
+                // chargingEffectMain.loop = false;
+                chargingEffect.Stop();
+            }
+            
+            if (fullChargeTime <= currentChargeTime)
+            {
+                spriteRenderer.sprite = Resources.Load<Sprite>("Sprites/Player_Charged");
+                // chargedEffect.Play();
+            }
         }
 
         if (Input.GetMouseButtonUp(0))
@@ -46,21 +64,20 @@ public class Player : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
             endPoint = m_camera.ScreenToWorldPoint(Input.mousePosition);
             endPoint.z = 1f;
 
-            force = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minDistance.x, maxDistance.x),
-                                Mathf.Clamp(startPoint.y - endPoint.y, minDistance.y, maxDistance.y));
+            distance = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minDistance.x, maxDistance.x),
+                                   Mathf.Clamp(startPoint.y - endPoint.y, minDistance.y, maxDistance.y));
 
-            m_rigidbody2D.velocity = force * moveSpeed;
-            chargeEffect.loop = false;
+            if (fullChargeTime <= currentChargeTime)
+            {
+                spriteRenderer.sprite = Resources.Load<Sprite>("Sprites/Player");
+                m_rigidbody2D.velocity = distance * speed;
+                currentChargeTime = 0f;
+            }
+            else
+            {
+                chargingEffect.Stop();
+                currentChargeTime = 0f;
+            }
         }
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        isMove = false;
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        isMove = false;
     }
 }
