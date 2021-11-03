595a44c70f3c3258ffe1a0e84cb39ac8c22ae763
simplex3510 <leeyj6457@naver.com>
2021-11-03 23:42:52 +0900

    리소스 수정 및 자잘한 경고 수정

--- Assets/Scripts/ChangeCursor.cs
+++ Assets/Scripts/ChangeCursor.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class ChangeCursor : MonoBehaviour
 {
-    [SerializeField]
     Texture2D cursorImage;
 
     void Start()
     {
-        Cursor.SetCursor(cursorImage, Vector2.zero, CursorMode.ForceSoftware);
+        cursorImage = Resources.Load<Texture2D>("Sprites/Cursor");
+        Cursor.SetCursor(cursorImage, Vector2.zero, CursorMode.Auto);
     }
 }
