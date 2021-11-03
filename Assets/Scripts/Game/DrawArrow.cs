fff32213834fc6057c02470c164a900fff41e5b1
simplex3510 <leeyj6457@naver.com>
2021-11-03 23:15:29 +0900

    적당히 손 봄

--- Assets/Scripts/Game/DrawArrow.cs
+++ Assets/Scripts/Game/DrawArrow.cs
@@ -8,22 +8,32 @@ public class DrawArrow : MonoBehaviour
     float minOffset = -5f;
     float maxOffset = 5f;
     LineRenderer lineRenderer;
+    Vector3[] points;
 
     private void Awake()
     {
+        points = new Vector3[2];
         lineRenderer = GetComponent<LineRenderer>();
     }
 
     public void RenderLine(Vector3 _startPoint, Vector3 _endPoint)
     {
         lineRenderer.positionCount = 2;
-        Vector3[] points = new Vector3[2];
+
         _startPoint = new Vector3(Mathf.Clamp(_startPoint.x, minOffset, maxOffset),
                                   Mathf.Clamp(_startPoint.y, minOffset, maxOffset),
                                   0f);
         _endPoint   = new Vector3(Mathf.Clamp(_endPoint.x, minOffset, maxOffset),
                                   Mathf.Clamp(_endPoint.y, minOffset, maxOffset),
                                   0f);
+
+        // 매그니튜드(벡터 길이)를 활용해서 clamp로 제한하면?
+        // _startPoint = new Vector3(Mathf.Clamp(_startPoint.magnitude, minOffset, maxOffset),
+        //                           Mathf.Clamp(_startPoint.magnitude, minOffset, maxOffset),
+        //                           0f);
+        // _endPoint   = new Vector3(Mathf.Clamp(_endPoint.magnitude, minOffset, maxOffset),
+        //                           Mathf.Clamp(_endPoint.magnitude, minOffset, maxOffset),
+        //                           0f);
 
         points[0] = _startPoint;
         points[1] = _endPoint;
