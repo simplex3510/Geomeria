cb9e5b1a7908f887d5ddf3be389a311bfcd2958c
simplex3510 <leeyj6457@naver.com>
2021-11-13 11:29:56 +0900

    에너미 스포너 원회전 및 오브젝트 풀링 시스템 구현중

--- Assets/Scripts/Game/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Game/Enemy/EnemySpawner.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField]
-    GameObject enemyPrefab;
     public float spawnRateMin;
     public float spawnRateMax;
+    public GameObject[] enemys;
 
     float spawnRate = 2f;
     float timeAfterSpawn;
@@ -15,7 +14,7 @@ public class EnemySpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -25,11 +24,10 @@ public class EnemySpawner : MonoBehaviour
 
         if(spawnRate <= timeAfterSpawn)
         {
+            if(enemys[Random.Range(0,10)].gameObject.activeSelf == true)
+
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
             timeAfterSpawn = 0;
-
-            var enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
-
-            enemy.transform.SetParent(null);
         }
         
     }
